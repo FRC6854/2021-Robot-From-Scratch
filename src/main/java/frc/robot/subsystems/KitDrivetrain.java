24ae6b4e525c369b1961e25d4e2be9373911a50e
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2022-07-08 19:25:34 -0400

    Add more helper functions to KitDrivetrain Subsystem

--- src/main/java/frc/robot/subsystems/KitDrivetrain.java
+++ src/main/java/frc/robot/subsystems/KitDrivetrain.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Robot;
@@ -15,6 +17,8 @@ public class KitDrivetrain extends SubsystemBase implements RobotMap {
 	private VikingSPX right_follower = null;
 	private DifferentialDrive diff_drive = null;
 
+	private AHRS gyroscope = null;
+
 	public KitDrivetrain() {
 		boolean is_inverted = Robot.isReal();
 
@@ -25,13 +29,18 @@ public class KitDrivetrain extends SubsystemBase implements RobotMap {
 		right_follower = new VikingSPX(CAN_RIGHT_BACK, right_master, is_inverted);
 
 		diff_drive = new DifferentialDrive(left_master, right_master);
+
+		gyroscope = new AHRS(Port.kMXP);
+		gyroscope.reset();
 	}
 
 	@Override
-	public void periodic() {}
+	public void periodic() {
+	}
 
 	@Override
-	public void simulationPeriodic() {}
+	public void simulationPeriodic() {
+	}
 
 	public static KitDrivetrain getInstance() {
 		if (instance == null) {
@@ -44,5 +53,37 @@ public class KitDrivetrain extends SubsystemBase implements RobotMap {
 	public void arcade_drive(double x, double z) {
 		diff_drive.curvatureDrive(x, z, true);
 		diff_drive.feed();
+	}
+
+	public boolean have_gyro() {
+		return gyroscope.isConnected();
+	}
+
+	public double gyro_get_pitch() {
+		return gyroscope.getPitch();
+	}
+
+	public double gyro_get_roll() {
+		return gyroscope.getRoll();
+	}
+
+	public double gyro_get_yaw() {
+		return gyroscope.getYaw();
+	}
+
+	public double get_left_master_ticks() {
+		return left_master.getTicks();
+	}
+
+	public double get_right_master_ticks() {
+		return right_master.getTicks();
+	}
+
+	public double get_left_master_velocity() {
+		return left_master.getVelocity();
+	}
+
+	public double get_right_master_velocity() {
+		return right_master.getVelocity();
 	}
 }
